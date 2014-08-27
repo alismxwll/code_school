feed03ccd7b99ed5773f8a8ca0a84fb74b917d3f
Meghan Lindsley & Andrew Akers <student@epicodus.com>
2014-08-27 14:23:28 -0700

    Add method for finding the next lesson

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -3,5 +3,9 @@ class Lesson < ActiveRecord::Base
   validates :content, :presence => true
   validates :lesson_number, :presence => true
 
-  before_save :to_integer
+  def next
+    next_number = self.lesson_number + 1
+    next_lesson = Lesson.find_by(:lesson_number => next_number)
+    next_lesson
+  end
 end
--- spec/lesson_spec.rb
+++ spec/lesson_spec.rb
@@ -4,4 +4,14 @@ describe Lesson do
   it { should validate_presence_of :name }
   it { should validate_presence_of :content }
   it { should validate_presence_of :lesson_number}
+
+  context '#next' do
+    it 'returns the lesson with the next-highest number than the current lesson' do
+      current_lesson = Lesson.create({name: 'lesson1', content: 'content', lesson_number: 1})
+      next_lesson = Lesson.create({name: 'lesson2', content: 'more content', lesson_number: 2})
+      expect(current_lesson.next).to eq next_lesson
+    end
+  end
 end
+
+
